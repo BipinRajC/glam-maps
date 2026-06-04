49c4cb2b8e57fe23a096e640ac9bdd8a1f295212
BipinRajC <bipinraj.4604@gmail.com>
2026-06-04 23:51:37 +0530

    refactor: rewrite JourneyScreen with ref-based animation, fix SSR via dynamic import

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,8 @@
-import GlamMaps from "@/components/GlamMaps";
+"use client";
+
+import dynamic from "next/dynamic";
+
+const GlamMaps = dynamic(() => import("@/components/GlamMaps"), { ssr: false });
 
 export default function Home() {
   return <GlamMaps />;
