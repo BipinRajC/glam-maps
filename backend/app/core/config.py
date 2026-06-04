85100e09c724b9566cb33d6a6c114bc247fb630e
Amol Vyas <amol.vyas0630@gmail.com>
2026-06-04 16:12:13 +0530

    added postGIS + docker-compose

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,8 +3,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = 'Glam Maps API'
+    DATABASE_URL: str = 'postgresql://postgres:password@localhost:5432/glammaps'
 
-    model_config = SettingsConfigDict(env_file='.env')
+    model_config = SettingsConfigDict(env_file='.env', extra='ignore')
 
 
 settings = Settings()
